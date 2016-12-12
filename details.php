9c50f7144bd596aa91538ea76145c68446136037
mmantle-hud <m.e.mantle@hud.ac.uk>
2016-12-12 17:31:10 +0000

    Added details

--- details.php
+++ details.php
@@ -1,28 +1,8 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<title>Film details</title>
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-</head>
-<body>
-<ul>
-	<li><a href="list.php">View all films</a></li>
-	<li><a href="insert-form.php">Add a new film</a></li>
-	<li><a href="delete-form.php">Delete films</a></li>
-</ul>
+
 <?php
 require_once("film-model.php");
 $filmId=$_GET['id'];
 $film=getFilmById($filmId);
-
-if($film){
-	echo "<h1>".$film['title']."</h1>";
-	echo "<ul>";
-	echo "<li>Year: ".$film['year']."</li>";
-	echo "<li>Duration: ".$film['duration']."</li>";
-	echo "</ul>";
-}
-
+$title="Film details";
+include("views/list-view.php");
 ?>
-</body>
-</html>
